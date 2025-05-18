c18155399cc0617455873a523dd1015cd45c5ba5
Bryan.dev <bryanmiranda.dev@gmail.com>
2025-05-18 02:42:57 +0000

    Nivel aventureiro adicionando navios diagonais

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,45 +4,58 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
+#define linhas 10
+#define colunas 10
+
 int main() {
-    // Nível Novato - Posicionamento dos Navios
      //Variáveis
-    int tabuleiro[10][10]; //Variável do tamanho do tabuleiro.
+    int tabuleiro[linhas][colunas]; //Variável do tamanho do tabuleiro.
     char posicao[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};// Variável de posições das colunas
     int i, j, letreiro; // Variáveis para loops
 
     // Determinando inicialização do tabuleiro
-    for(i = 0; i < 10; i++){
-        for(j = 0; j < 10; j++){
+    for(i = 0; i < linhas; i++){
+        for(j = 0; j < colunas; j++){
             tabuleiro[i][j] = 0;
         }
     }
 
     //Inicializando posições dos navios
     // Horizontal
-    tabuleiro[2][3] = 3;
-    tabuleiro[2][4] = 3;
-    tabuleiro[2][5] = 3;
+    for(int m = 2, n = 3; n < 6; n++){
+        tabuleiro[m][n] = 3;
+    }
 
     // Vertical
-    tabuleiro[5][7] = 3;
-    tabuleiro[6][7] = 3;
-    tabuleiro[7][7] = 3;
+    for(int o = 5, p = 7; o < 8; o++){
+        tabuleiro[o][p] = 3;
+    }
+
+    // Diagonal
+    for(int x = 3, y = 2; x < linhas - 4; x++, y++){
+        tabuleiro[x][y] = 3;
+    }
+
+    // Diagonal 2
+    for(int v = 8, z = 1; z < 4; v--, z++){
+        tabuleiro[v][z] = 3;
+    }
+
 
     // Espaço para ficar confortável esteticamente.
     printf(" ");
 
     // loop para mostrar array de letras do tabuleiro
-    for(letreiro = 0; letreiro < 10; letreiro++){
+    for(letreiro = 0; letreiro < colunas; letreiro++){
         printf("%2c", posicao[letreiro]);
     }
 
     printf("\n");
 
-    for(i = 0; i < 10; i++){
+    for(i = 0; i < linhas; i++){
         printf("%d", i);
 
-        for(j = 0; j < 10; j++){
+        for(j = 0; j < colunas; j++){
             
             //Imprimindo tabuleiro
             printf("%2d", tabuleiro[i][j]);
@@ -50,11 +63,7 @@ int main() {
         printf("\n");
     }
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
+    
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
     // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
